489aa3be77cf06ad2a942f8280230ee56b00ccfa
Dan Stachlewski <dan.stachlewski@gmail.com>
2016-11-23 11:03:20 +1100

    This is the initial commit for St. Andrew's Parish of Aberfeldie website in Drupal 7. This is up-to-date (utd) as of 2016.11.23.

---
+++ sites/all/themes/stAndrewsChurch/template.php
@@ -0,0 +1,202 @@
+<?php
+/**
+ * @file(template.php) file for stAndrewsChurch
+ *
+ * Implement Preprocess Functions and alter Hooks in this file.
+ */
+
+/**
+ * Preprocess function for page.tpl.php
+ */
+
+function stAndrewsChurch_preprocess_page(&$variables) {
+
+/* How to check for Variables being printed/used on the page: */
+/* Override the Site Slogan & Format to Fit in Header */
+$variables['site_slogan'] = "\"For surely I know the plans I have for you, </br>says the LORD, plans for your welfare and</br> not for harm, to give you a future with hope\". </br><strong>GOD'S PROMISE TO US </br>- Jeremiah 29:11 (NRSV)</strong>";
+/* Override the Main Site Name & Format to Fit in Header */
+$variables['site_name'] = "St. Andrew's Anglican <br>Parish of Aberfeldie";
+//kpr($variables);
+
+/**
+ * How to implement:
+ * [changing random Slogans on every page visit.]
+ * t() function used for multi-lingal sites that transalte strings
+ *
+ */
+/*
+$slogans = array();
+$slogans[] = t('Dummy Slogan');
+*/
+/* Array of Slogans to use:
+$slogans = array(
+		t('Arise my soul, and review your deeds which have preceeded from you.  </br>Scrutinize them closely, and shed the rain of your tears, declaring openly </br>to Christ your thoughts and deeds, so that you may be justified. </br><strong>Saint Andrew of Crete</strong>'),
+		t('Love is acceptance. When you love someone . . . </br>you take them into your heart, and that is surely </br>why it hurts so much when we lose someone we love, </br>because we lose a part of ourselves.</br><strong>Saint Andrew the Apostle</strong>'),
+		t('If we strive for goals, relishing in the pleasure of circumstance, </br>nothing is enjoyable, and life becomes purposeless.</br><strong>Saint Andrew the Apostle</strong>'),
+	);
+*/
+/**
+ * Select a random slogan from the array/list and set the $site_slogan variable to the new random slogan
+ *
+
+$slogan = $slogans[array_rand($slogans)];
+
+$variables['site_slogan'] = $slogan;
+kpr($variables);
+*/
+}
+
+/**
+ * Add body classes if certain regions have content.
+ */
+function stAndrewsChurch_preprocess_html(&$variables) {
+  if (!empty($variables['page']['featured'])) {
+    $variables['classes_array'][] = 'featured';
+  }
+
+  if (!empty($variables['page']['triptych_first'])
+    || !empty($variables['page']['triptych_middle'])
+    || !empty($variables['page']['triptych_last'])) {
+    $variables['classes_array'][] = 'triptych';
+  }
+
+  if (!empty($variables['page']['footer_firstcolumn'])
+    || !empty($variables['page']['footer_secondcolumn'])
+    || !empty($variables['page']['footer_thirdcolumn'])
+    || !empty($variables['page']['footer_fourthcolumn'])) {
+    $variables['classes_array'][] = 'footer-columns';
+  }
+
+  // Add conditional stylesheets for IE
+  drupal_add_css(path_to_theme() . '/css/ie.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'preprocess' => FALSE));
+  drupal_add_css(path_to_theme() . '/css/ie6.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'IE 6', '!IE' => FALSE), 'preprocess' => FALSE));
+}
+
+/**
+ * Override or insert variables into the page template for HTML output.
+ */
+function stAndrewsChurch_process_html(&$variables) {
+  // Hook into color.module.
+  if (module_exists('color')) {
+    _color_html_alter($variables);
+  }
+}
+
+/**
+ * Override or insert variables into the page template.
+ */
+function stAndrewsChurch_process_page(&$variables) {
+  // Hook into color.module.
+  if (module_exists('color')) {
+    _color_page_alter($variables);
+  }
+  // Always print the site name and slogan, but if they are toggled off, we'll
+  // just hide them visually.
+  $variables['hide_site_name']   = theme_get_setting('toggle_name') ? FALSE : TRUE;
+  $variables['hide_site_slogan'] = theme_get_setting('toggle_slogan') ? FALSE : TRUE;
+  if ($variables['hide_site_name']) {
+    // If toggle_name is FALSE, the site_name will be empty, so we rebuild it.
+    $variables['site_name'] = filter_xss_admin(variable_get('site_name', 'Drupal'));
+  }
+  if ($variables['hide_site_slogan']) {
+    // If toggle_site_slogan is FALSE, the site_slogan will be empty, so we rebuild it.
+    $variables['site_slogan'] = filter_xss_admin(variable_get('site_slogan', ''));
+  }
+  // Since the title and the shortcut link are both block level elements,
+  // positioning them next to each other is much simpler with a wrapper div.
+  if (!empty($variables['title_suffix']['add_or_remove_shortcut']) && $variables['title']) {
+    // Add a wrapper div using the title_prefix and title_suffix render elements.
+    $variables['title_prefix']['shortcut_wrapper'] = array(
+      '#markup' => '<div class="shortcut-wrapper clearfix">',
+      '#weight' => 100,
+    );
+    $variables['title_suffix']['shortcut_wrapper'] = array(
+      '#markup' => '</div>',
+      '#weight' => -99,
+    );
+    // Make sure the shortcut link is the first item in title_suffix.
+    $variables['title_suffix']['add_or_remove_shortcut']['#weight'] = -100;
+  }
+}
+
+/**
+ * Implements hook_preprocess_maintenance_page().
+ */
+function stAndrewsChurch_preprocess_maintenance_page(&$variables) {
+  // By default, site_name is set to Drupal if no db connection is available
+  // or during site installation. Setting site_name to an empty string makes
+  // the site and update pages look cleaner.
+  // @see template_preprocess_maintenance_page
+  if (!$variables['db_is_active']) {
+    $variables['site_name'] = '';
+  }
+  drupal_add_css(drupal_get_path('theme', 'stAndrewsChurch') . '/css/maintenance-page.css');
+}
+
+/**
+ * Override or insert variables into the maintenance page template.
+ */
+function stAndrewsChurch_process_maintenance_page(&$variables) {
+  // Always print the site name and slogan, but if they are toggled off, we'll
+  // just hide them visually.
+  $variables['hide_site_name']   = theme_get_setting('toggle_name') ? FALSE : TRUE;
+  $variables['hide_site_slogan'] = theme_get_setting('toggle_slogan') ? FALSE : TRUE;
+  if ($variables['hide_site_name']) {
+    // If toggle_name is FALSE, the site_name will be empty, so we rebuild it.
+    $variables['site_name'] = filter_xss_admin(variable_get('site_name', 'Drupal'));
+  }
+  if ($variables['hide_site_slogan']) {
+    // If toggle_site_slogan is FALSE, the site_slogan will be empty, so we rebuild it.
+    $variables['site_slogan'] = filter_xss_admin(variable_get('site_slogan', ''));
+  }
+}
+
+/**
+ * Override or insert variables into the node template.
+ */
+function stAndrewsChurch_preprocess_node(&$variables) {
+  if ($variables['view_mode'] == 'full' && node_is_page($variables['node'])) {
+    $variables['classes_array'][] = 'node-full';
+  }
+}
+
+/**
+ * Override or insert variables into the block template.
+ */
+function stAndrewsChurch_preprocess_block(&$variables) {
+  // In the header region visually hide block titles.
+  if ($variables['block']->region == 'header') {
+    $variables['title_attributes_array']['class'][] = 'element-invisible';
+  }
+}
+
+/**
+ * Implements theme_menu_tree().
+ */
+function stAndrewsChurch_menu_tree($variables) {
+  return '<ul class="menu clearfix">' . $variables['tree'] . '</ul>';
+}
+
+/**
+ * Implements theme_field__field_type().
+ */
+function stAndrewsChurch_field__taxonomy_term_reference($variables) {
+  $output = '';
+
+  // Render the label, if it's not hidden.
+  if (!$variables['label_hidden']) {
+    $output .= '<h3 class="field-label">' . $variables['label'] . ': </h3>';
+  }
+
+  // Render the items.
+  $output .= ($variables['element']['#label_display'] == 'inline') ? '<ul class="links inline">' : '<ul class="links">';
+  foreach ($variables['items'] as $delta => $item) {
+    $output .= '<li class="taxonomy-term-reference-' . $delta . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</li>';
+  }
+  $output .= '</ul>';
+
+  // Render the top-level DIV.
+  $output = '<div class="' . $variables['classes'] . (!in_array('clearfix', $variables['classes_array']) ? ' clearfix' : '') . '"' . $variables['attributes'] .'>' . $output . '</div>';
+
+  return $output;
+}
